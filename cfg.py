585dafe6a43045a752ed491a8eb88e75e62e13c0
harrylloyd-bl <harry.lloyd@bl.uk>
2025-08-26 16:29:27 +0100

    fix path to lang code json

--- cfg.py
+++ cfg.py
@@ -1,6 +1,6 @@
 import json
 
-LANG_DICT = json.load(open("data/raw/marc_lang_codes.json", "r"))
+LANG_DICT = json.load(open("data/external/marc_lang_codes.json", "r"))
 P5_ROOT = (
     "G:/DigiSchol/Digital Research and Curator Team/Projects & Proposals/00_Current Projects"
     "/LibCrowds Convert-a-Card (Adi)/OCR/20230504 TKB Export P5 175 GT pp/1016992/P5_for_Transkribus"
